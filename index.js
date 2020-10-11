dc73175eb327c12ddc3ca144668eb62562198eec
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-10-11 18:46:42 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = () => {
+    // let a = 2000
+    // let b = 100
+    // let  c = a - b
+return 2000/100
+}
+
+const square = (a) => {
+    return a*a
+}
+
+const add = (a, b) => {
+    return a+b
+}
